8c79c18faa8579f92aefa539ad04d9820e03322d
Amantina Sosa <amantinasosa@gmail.com>
2020-09-13 10:49:57 -0400

    base url adjusted

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseUrl: 'http://localhost:5001/clone-33d8a/us-central1/api', //the api cloud function url
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
-  },
+  // baseUrl: 'http://localhost:5001/clone-33d8a/us-central1/api', //the api cloud function url
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
+  // },
 
-  // baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api',
+  baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api',
 });
 
 export default instance;
