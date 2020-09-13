439617e28af9fece41849a7033c4be8fe548a890
Amantina Sosa <amantinasosa@gmail.com>
2020-09-13 10:52:12 -0400

    updates

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './CheckoutProduct.css';
 import { useStateValue } from './StateProvider';
 
-function CheckoutProduct({ id, title, image, price, rating }) {
+function CheckoutProduct({ id, title, image, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
   const removeFromBasket = () => {
     dispatch({
@@ -30,8 +30,9 @@ function CheckoutProduct({ id, title, image, price, rating }) {
               <p>⭐</p>
             ))}
         </div>
-
-        <button onClick={removeFromBasket}> Remove from basket</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}> Remove from basket</button>
+        )}
       </div>
     </div>
   );
