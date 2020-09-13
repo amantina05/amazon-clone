a87ff693b90b1d9a7b7d6d725149f8bf961921ef
Amantina Sosa <amantinasosa@gmail.com>
2020-09-13 10:45:25 -0400

    route for orders added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import Orders from './Orders';
 import Checkout from './Checkout';
+import Payment from './Payment';
 import Login from './Login';
 import { useStateValue } from './StateProvider';
 import { auth } from './firebase';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe(
+  'pk_test_51HQCBaGHNWsy1zYawxlRIvG3w9hhW4dqbugq48tYvZkghbjhgrSp87mpu244XFFxwPkOPyVOpxSOPSpIIcEVYmsz00yWvShjFH'
+);
 
 function App() {
   const [{ basket }, dispatch] = useStateValue();
@@ -41,9 +49,19 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/login">
             <Login />
